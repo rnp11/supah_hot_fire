d4b49584e41387a9fa30395fcce2e964e09f49b4
Adrian Deleon <amd87@students.uwf.edu>
2017-04-09 01:21:11 -0500

    database querying setting up

---
+++ CodeFest/Infinity/Infinity/Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SQLite;
+
+namespace Infinity.Models
+{
+    public class DatabaseManager
+    {
+        public makeQuery()
+        {
+            SQLiteConnection conn = new SQLiteConnection("Data Source=C:/Users/nacht_000/Documents/GitHub/supah_hot_fire/CodeFest/Infinity/Infinity/Database.db;");
+
+            conn.Open();
+        }
+        
+    }
+}
