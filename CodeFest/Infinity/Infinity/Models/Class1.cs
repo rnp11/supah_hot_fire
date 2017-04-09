007bed7048c6106502316e6a16a7eb5560701810
Adrian Deleon <amd87@students.uwf.edu>
2017-04-09 01:48:00 -0500

    done????

--- CodeFest/Infinity/Infinity/Models/Class1.cs
+++ CodeFest/Infinity/Infinity/Models/Class1.cs
@@ -8,11 +8,14 @@ namespace Infinity.Models
 {
     public class DatabaseManager
     {
-        public makeQuery()
+        public SQLiteDataReader makeQuery(string command)
         {
             SQLiteConnection conn = new SQLiteConnection("Data Source=C:/Users/nacht_000/Documents/GitHub/supah_hot_fire/CodeFest/Infinity/Infinity/Database.db;");
-
             conn.Open();
+            SQLiteCommand comm = new SQLiteCommand(command, conn);
+            SQLiteDataReader reader = comm.ExecuteReader();
+            conn.Close();
+            return reader;
         }
         
     }
